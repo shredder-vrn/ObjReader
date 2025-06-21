122ee82f7de0c63f7f02e66158182ce4bec3b162
shredder-vrn <p_shcherbatov@mail.ru>
2025-06-21 16:08:15 +0300

    Commit #15, остановлюсь на сегодня

--- test/obj_reader/objreadertest.cpp
+++ test/obj_reader/objreadertest.cpp
@@ -1,1 +1,43 @@
 #include "objreadertest.h"
+#include "../src/obj_reader/meshdata.h"
+#include "../src/obj_reader/objreader.h"
+
+#include <QTest>
+
+
+void ObjReaderTest::testParseVertexLine_data()
+{
+    QTest::addColumn<QStringList>("tokens");
+    QTest::addColumn<bool>("expertedResult");
+
+    QTest::newRow("valid vertex") << QStringList{"v", "1.0", "2.0", "3.0"} << true;
+    QTest::newRow("inbalid y coord") << QStringList{"v", "asd", "2.0", "3.0"} << false;
+    QTest::newRow("not number z") << QStringList{"v", "1.0", "2.0", ""} << false;
+    QTest::newRow("too few tokens") << QStringList{"v", "1.0", "2.0"} << true;
+}
+
+void ObjReaderTest::testParseVertexLine()
+{
+    QFETCH(QStringList, tokens);
+    QFETCH(bool, expertedResult);
+
+    Model model;
+   // bool result = parseVertexLine(tokens, "", 1, model);
+
+
+}
+
+void ObjReaderTest::testParseFaceLine1()
+{
+
+}
+
+void ObjReaderTest::testParseFaceLine2()
+{
+
+}
+
+void ObjReaderTest::testParseFaceLine3()
+{
+
+}
--- test/obj_reader/objreadertest.h
+++ test/obj_reader/objreadertest.h
@@ -1,4 +1,19 @@
 #ifndef OBJREADERTEST_H
 #define OBJREADERTEST_H
 
+#include <QObject>
+
+class ObjReaderTest : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testParseVertexLine_data();
+    void testParseVertexLine();
+
+    void testParseFaceLine1();
+    void testParseFaceLine2();
+    void testParseFaceLine3();
+};
+
 #endif // OBJREADERTEST_H
